8124552a54e593b64abec9a7e0c1a224be9e20bb
Shifty <162399486+ShiftyIsMine@users.noreply.github.com>
2025-08-25 11:46:39 +0900

    jdjdjdbc

---
+++ src/main/java/kr/ac/kopo/shifty/fefeefef/FefeefefApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.shifty.fefeefef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FefeefefApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FefeefefApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/kopo/shifty/fefeefef/FefeefefApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.shifty.fefeefef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FefeefefApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
